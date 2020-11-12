e8da57d139102f61411258d331de78952cc27667
unknown <yosibukris@gmail.com>
2020-11-12 15:34:32 +0200

    noyhnig

--- src/main/java/acs/logic/EnhancedUserService.java
+++ src/main/java/acs/logic/EnhancedUserService.java
@@ -1,9 +1,8 @@
 package acs.logic;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnhancedUserService extends UserService, CrudRepository<T, ID>{
+public interface EnhancedUserService extends UserService{
 
 }
